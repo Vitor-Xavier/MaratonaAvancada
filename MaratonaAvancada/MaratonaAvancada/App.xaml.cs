87c4dd9113a49ef213142dbae8ab9d25304ff3c0
Vitor Xavier de Souza <vitorvxs@live.com>
2018-06-05 15:46:44 -0300

    Add Value Converter

--- MaratonaAvancada/MaratonaAvancada/App.xaml.cs
+++ MaratonaAvancada/MaratonaAvancada/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MaratonaAvancada
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart ()
