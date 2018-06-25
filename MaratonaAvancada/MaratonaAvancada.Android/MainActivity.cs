2a9c0ea7a950eb189ace49834e4afa2a4158f1d6
Vitor Xavier de Souza <vitorvxs@live.com>
2018-06-25 13:32:37 -0300

    AppLinks Modulo 7

--- MaratonaAvancada/MaratonaAvancada.Android/MainActivity.cs
+++ MaratonaAvancada/MaratonaAvancada.Android/MainActivity.cs
@@ -7,10 +7,16 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Xamarin.Forms;
+//using Xamarin.Forms.Platform.Android.AppLinks;
 
 namespace MaratonaAvancada.Droid
 {
     [Activity(Label = "MaratonaAvancada", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+        DataScheme = "maratonaxamarinTst",
+        DataHost = "maratonaxamarinTst.com.br",
+        DataPathPrefix = "/",
+        Categories = new [] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -21,6 +27,8 @@ namespace MaratonaAvancada.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            //Xamarin Forms AppLinks
+            //AndroidAppLinks.Init(this);
             LoadApplication(new App());
 
             MessagingCenter.Subscribe<Models.Message>(this, "SendMessage", message =>
--- MaratonaAvancada/MaratonaAvancada/ControlsPage.xaml.cs
+++ MaratonaAvancada/MaratonaAvancada/ControlsPage.xaml.cs
@@ -44,5 +44,23 @@ namespace MaratonaAvancada
                 Avatar = "https://orig00.deviantart.net/3ce4/f/2015/207/d/0/steam_themed_smiley_face_avatar_by_yirktos-d92z52h.png"
             });
         }
+
+        public static void CreateIndex(Models.Person person)
+        {
+            var template = "https://maratonaxamarinTst.com.br/?PersonId={0}";
+            var entry = new AppLinkEntry
+            {
+                Title = person.Name,
+                Description = person.Bio.Substring(0, 30),
+                AppLinkUri = new Uri(string.Format(template, person.PersonId), UriKind.RelativeOrAbsolute),
+                IsLinkActive = true,
+                Thumbnail = ImageSource.FromUri(new Uri(person.Avatar))
+            };
+            entry.KeyValues.Add("contentType", "User");
+            entry.KeyValues.Add("appName", "MaratonaXamarin");
+            entry.KeyValues.Add("companyName", "Vitor Xavier");
+
+            Application.Current.AppLinks.RegisterLink(entry);
+        }
 	}
 }
